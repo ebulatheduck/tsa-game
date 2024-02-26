15dc3d5ac212963cd2497da877061e8ed271a293
Ebula Duck <ebula.the.duck@gmail.com>
2024-02-26 14:41:19 -0700

    first REAL test

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+'use strict';
+let player, cursors;
+
 class Scene extends Phaser.Scene {
     preload = () => {
+        cursors = this.input.keyboard.createCursorKeys();
+
+        // TODO: Replace test images with actual sprites
         this.load.image('chrome', 'assets/chrome-blank.png');
         this.load.image('train', 'assets/image0-17.png');
         this.load.image('ios', 'assets/ios_sample.png');
     }
 
     create = () => {
-        this.add.image(800, 600, 'chrome');
-        this.add.image(300, 500, 'ios').setScale(4, 2);
+        this.add.image(800, 500, 'chrome');
+        player = this.physics.add.image(300, 100, 'ios');
+        player.setCollideWorldBounds(true);
+        player.setBounce(0.1);
     }
 
     update = () => {
+        player.setVelocityX(
+            cursors.left.isDown ? -200 :
+            cursors.right.isDown ? 200 : 0);
+
+        if (cursors.up.isDown && player.body.blocked.down) {
+            player.setVelocityY(-330);
+        }
     }
 }
 
